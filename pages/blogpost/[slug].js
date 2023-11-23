2dfa9c2809ef4e8bdadf390faac941e4a857f7ac
dodoextinct <yashkmkrishan@gmail.com>
2023-11-23 10:14:13 +0530

    added fetching of blogs in blogpost files

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,22 +1,33 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import styles from '../../styles/blogpost.module.css';
 
 const slug= () => {
-    const router = useRouter();
-    console.log(router.query);
-    const {slug} = router.query || {};
+  const [blogs, setBlogs] = useState([]);
+  const router = useRouter();
+  console.log(router.query);
+  const {slug} = router.query || {};
+
+  useEffect(() => {
+      if (!router.isReady) return;
+      console.log("useeffect is running");
+      fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
+          return a.json();
+      })
+      .then((parsed) => {
+          setBlogs(parsed)
+      })
+    }, [router.isReady])
 
   return (
     <div>
       <div className={styles.title}>
-        <h2>Let us discuss {slug}</h2>
+        <h2>{blogs && blogs.heading}</h2>
       </div>
       <hr className={styles.container}></hr>
       <div className={styles.container}>
       <p>
-      The term "dodo" is often used metaphorically, typically referring to someone acting in a foolish or outdated manner. However, if you're referring to the extinct bird, the dodo, being a dodo isn't something one can literally become, as it was a unique species native to Mauritius. The dodo became a symbol for obsolescence and extinction due to its disappearance in the late 17th century, often attributed to human activity.
-If you're using the term in a metaphorical sense and want to avoid being seen as a "dodo," it generally means staying informed, adaptable, and open to new ideas, avoiding outdated or ineffective practices.
+        {blogs && blogs.content}
       </p>
       </div>
     </div>
